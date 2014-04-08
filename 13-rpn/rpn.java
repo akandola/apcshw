3305bd66f9d64c7e896fcb37ba42053bac78773e
Anmolpreet Kandola <akandola97@gmail.com>
2014-04-08 09:01:39 -0400

    rpn works

--- 13-rpn/rpn.java
+++ 13-rpn/rpn.java
@@ -2,34 +2,83 @@ import java.io.*;
 import java.util.*;
 
 public class rpn {
-    rpn a = new rpn();
+ private int top;
+    private String[] stak;
+    private int numElts;
+
+    public rpn() {
+	stak = new String[10];
+	top = -1;
+	numElts = 0;
+    }
+
+    private void grow() {
+	String[] ans = new String[stak.length * 2];
+	for (int i=0;i<stak.length;i++) {
+	    ans[i] = stak[i];
+	}
+	    stak = ans;
+    }
+
+    public void push(String s) {
+	top++;
+	while(top>=stak.length) {
+	    grow();
+	}
+	stak[top] = s;
+	numElts++;
+    }
+
+    public String pop() {
+	String temp = new String(stak[top]);
+	top = top-1;
+	numElts--;
+	return temp;
+    }
+
+    public String peek() {
+	return stak[top];
+    }
+    
+    public int size() {
+	return numElts;
+    }
+
+    public String toString() {
+	String ans = new String();
+	for (int i=top;i>=0;i--) {
+	    ans = ans + String.format("%s\n",stak[i]);
+	}
+	return ans;
+    }
+
     public void calc(String s) {
 
 	//parseDouble returns a primitive double containing the value of a string
 	if (s.equals("+")) {
-	    double temp = Double.parseDouble(s.pop());
-	    double ans = temp + Double.parseDouble(s.pop());    
-	    s.push("" + ans);
+	    double temp = Double.parseDouble(this.pop());
+	    double ans = temp + Double.parseDouble(this.pop());    
+	    this.push("" + ans);
 	}
 
 	else if(s.equals("*")){
-	    double temp = Double.parseDouble(s.pop());
-	    double ans = temp * Double.parseDouble(s.pop());
+	    double temp = Double.parseDouble(this.pop());
+	    double ans = temp * Double.parseDouble(this.pop());
 	    this.push("" + ans);
 	}
 	else if(s.equals("-")){
-	    double temp = Double.parseDouble(s.pop());
-	    double ans = temp - Double.parseDouble(s.pop());
-	    s.push("" + ans);
+	    double temp = Double.parseDouble(this.pop());
+	    double ans = temp - Double.parseDouble(this.pop());
+	    this.push("" + ans);
 	}
 	else if (s.equals("/")){
-	    double temp = Double.parseDouble(s.pop());
-	    double ans = temp / Double.parseDouble(s.pop());
-	    s.push("" + ans);
+	    double temp = Double.parseDouble(this.pop());
+	    double ans = temp / Double.parseDouble(this.pop());
+	    this.push("" + ans);
 
 	}
 	else{
-	    s.push(s);
+	    this.push(s);
 	}
     }
     
